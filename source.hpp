9fddfccf9d61e9852c5d15c4f111c4b39b7ac74a
Dvirm97 <38911355+Dvirm97@users.noreply.github.com>
2020-01-14 14:18:10 +0200

    ~1~

--- source.hpp
+++ source.hpp
@@ -15,69 +15,69 @@ using namespace std;
 class Type{
     string* type;
 public:
-	string& get_type() const{
-		return *type;
-	}
-	void set_type(const string& other_type){
+    string& get_type() const{
+        return *type;
+    }
+    void set_type(const string& other_type){
         type = new string(other_type);
-	}
+    }
 };
 
 class Num: public Type{
-	string* value_str;
+    string* value_str;
 public:
-	void set_num(const string& num){
-		value_str = new string(num);
-		int value_int = stoi(num);
-		if (value_int <= 255){
-			set_type("BYTE");
-		}
-		else{
-			set_type("INT");
-		}
-	}
-	
-	string& get_value() const{
-		return *value_str;
-	}
+    void set_num(const string& num){
+        value_str = new string(num);
+        int value_int = stoi(num);
+        if (value_int <= 255){
+            set_type("BYTE");
+        }
+        else{
+            set_type("INT");
+        }
+    }
+
+    string& get_value() const{
+        return *value_str;
+    }
 };
 
 class EnumType: public Type{};
 
 class Statement: public Type{
-	vector<pair<int,BranchLabelIndex>>* true_list;
-	vector<pair<int,BranchLabelIndex>>* false_list;
-	vector<pair<int,BranchLabelIndex>>* next_list;
-public:	
+    vector<pair<int,BranchLabelIndex>>* true_list;
+    vector<pair<int,BranchLabelIndex>>* false_list;
+    vector<pair<int,BranchLabelIndex>>* next_list;
+public:
 
     void set_type2(const string& type1, const string& type2){
         if (type1!="VOID" ){
-			set_type(type1); 
-		}
-		else if(type2!="VOID" ){
-			set_type(type2); 
-		}
-		else{
-			set_type("VOID");
-		}
+            set_type(type1);
+        }
+        else if(type2!="VOID" ){
+            set_type(type2);
+        }
+        else{
+            set_type("VOID");
+        }
     }
-	vector<pair<int,BranchLabelIndex>>& get_true_list(){
-		return *true_list;
-	}
-	vector<pair<int,BranchLabelIndex>>& get_false_list(){
-		return *false_list;
-	}
-	void reset(){
-		true_list = new vector<pair<int,BranchLabelIndex>>();
-		false_list = new vector<pair<int,BranchLabelIndex>>();
-		next_list = new vector<pair<int,BranchLabelIndex>>();
-	}
-	void set_next_list(vector<pair<int,BranchLabelIndex>>& list){
-		next_list = new vector<pair<int,BranchLabelIndex>>(list);
-	}
-	vector<pair<int,BranchLabelIndex>>& get_next_list(){
-		return *next_list;
-	}
+    vector<pair<int,BranchLabelIndex>>& get_true_list(){
+        return *true_list;
+    }
+    vector<pair<int,BranchLabelIndex>>& get_false_list(){
+        return *false_list;
+    }
+    void reset(){
+        true_list = new vector<pair<int,BranchLabelIndex>>();
+        false_list = new vector<pair<int,BranchLabelIndex>>();
+        next_list = new vector<pair<int,BranchLabelIndex>>();
+    }
+    void set_next_list(vector<pair<int,BranchLabelIndex>>& list){
+        next_list = new vector<pair<int,BranchLabelIndex>>(list);
+    }
+    vector<pair<int,BranchLabelIndex>>& get_next_list(){
+        return *next_list;
+    }
 
 };
 
@@ -98,50 +98,50 @@ public:
 
 
 class Exp: public Type{
-	string* enum_value;
-	string* place;
+    string* enum_value;
+    string* place;
 public:
-	void set_enumVal(const string& enumVal){
+    void set_enumVal(const string& enumVal){
         enum_value = new string(enumVal);
     }
-	void set_place(const string& new_place){
-		place = new string(new_place);
-	}
-	 string& get_enumVal() const{
+    void set_place(const string& new_place){
+        place = new string(new_place);
+    }
+    string& get_enumVal() const{
         return *enum_value;
     }
-	string& get_place() const{
-		return *place;
-	}
-	void reset(){
-		enum_value = new string();
-		place = new string();
-	}
+    string& get_place() const{
+        return *place;
+    }
+    void reset(){
+        enum_value = new string();
+        place = new string();
+    }
 };
 
 class Call: public Exp{};
 
 class BoolExp: public Exp{
-	vector<pair<int,BranchLabelIndex>>* true_list;
-	vector<pair<int,BranchLabelIndex>>* false_list;
+    vector<pair<int,BranchLabelIndex>>* true_list;
+    vector<pair<int,BranchLabelIndex>>* false_list;
 public:
-	void set_true_list(vector<pair<int,BranchLabelIndex>>& item){
-		true_list = new vector<pair<int,BranchLabelIndex>>(item);
-	}
-	void set_false_list(vector<pair<int,BranchLabelIndex>>& item){
-		false_list = new vector<pair<int,BranchLabelIndex>>(item);
-	}
-	vector<pair<int,BranchLabelIndex>>& get_true_list(){
-		return *true_list;
-	}
-	vector<pair<int,BranchLabelIndex>>& get_false_list(){
-		return *false_list;
-	}
-	void BoolExp_reset(){
-		reset();
-		true_list = new vector<pair<int,BranchLabelIndex>>();
-		false_list = new vector<pair<int,BranchLabelIndex>>();
-	}
+    void set_true_list(vector<pair<int,BranchLabelIndex>>& item){
+        true_list = new vector<pair<int,BranchLabelIndex>>(item);
+    }
+    void set_false_list(vector<pair<int,BranchLabelIndex>>& item){
+        false_list = new vector<pair<int,BranchLabelIndex>>(item);
+    }
+    vector<pair<int,BranchLabelIndex>>& get_true_list(){
+        return *true_list;
+    }
+    vector<pair<int,BranchLabelIndex>>& get_false_list(){
+        return *false_list;
+    }
+    void BoolExp_reset(){
+        reset();
+        true_list = new vector<pair<int,BranchLabelIndex>>();
+        false_list = new vector<pair<int,BranchLabelIndex>>();
+    }
 };
 
 class Enumerator: public Id{};
@@ -189,7 +189,7 @@ public:
             params_type->push_back(it.get_type());
         return *params_type;
     }
-	vector<string>& get_params_ids() {
+    vector<string>& get_params_ids() {
         vector<string>* params_ids = new vector<string>();
         for(auto & it : *formals_list)
             params_ids->push_back(it.get_id());
@@ -230,47 +230,49 @@ public:
 };
 
 class M{
-	string* quad;
-	
+    string* quad;
+
 public:
-	void set_quad(string& label){
-		quad = new string(label);
-	}
-	string& get_quad(){
-		return *quad;
-	}
+    void set_quad(string& label){
+        quad = new string(label);
+    }
+    string& get_quad(){
+        return *quad;
+    }
 };
 
 class N{
-	vector<pair<int,BranchLabelIndex>>* next_list;
-	
+    vector<pair<int,BranchLabelIndex>>* next_list;
+
 public:
-	void set_next_list(vector<pair<int,BranchLabelIndex>>& list){
-		next_list = new vector<pair<int,BranchLabelIndex>>(list);
-	}
-	vector<pair<int,BranchLabelIndex>>& get_next_list(){
-		return *next_list;
-	}
+    void set_next_list(vector<pair<int,BranchLabelIndex>>& list){
+        next_list = new vector<pair<int,BranchLabelIndex>>(list);
+    }
+    vector<pair<int,BranchLabelIndex>>& get_next_list(){
+        return *next_list;
+    }
 };
 
 union STYPE
 {
     Type type;
-	Num num;
-	Exp exp;
-	BoolExp bool_exp;
+    Num num;
+    Exp exp;
+    BoolExp bool_exp;
     Call call;
     EnumType enum_type;
     Statement statement;
-	Statements statements;
-	RetType ret_type;
-	FormalDecl formal_decl;
-	Id id;
+    Statements statements;
+    RetType ret_type;
+    FormalDecl formal_decl;
+    Id id;
     Enumerator enumerator;
-	ExpList exp_list;
-	FormalsList formals_list;
-	Formals formals;
+    ExpList exp_list;
+    FormalsList formals_list;
+    Formals formals;
     EnumeratorList enumerator_list;
+    M m;
+    N n;
 } ;
 
 #define YYSTYPE STYPE
